84c1562fd34f251f6d14f77f8ff60de46d6c7b50
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-06-13 17:12:29 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management import file_management as my_function
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in instance.queue:
+        return None
+
+    file_list = my_function.txt_importer(path_file)
+    instance.enqueue(file_list)
+    print(
+        {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_list),
+            "linhas_do_arquivo": file_list,
+        }
+    )
 
 
 def remove(instance):
