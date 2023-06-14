f0dfb0c173a64722be79855fd3945703e810b7b4
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-06-14 17:01:50 -0300

    fix: linter

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,9 +1,9 @@
 from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
+
 def test_basic_priority_queueing():
     priority_queue = PriorityQueue()
-
 
     assert len(priority_queue) == 0
     priority_queue.enqueue({"qtd_linhas": 3})
@@ -17,15 +17,12 @@ def test_basic_priority_queueing():
 
     priority_queue.enqueue({"qtd_linhas": 4})
     priority_queue.enqueue({"qtd_linhas": 7})
-  
 
     assert priority_queue.search(0) == {"qtd_linhas": 4}
     assert priority_queue.search(1) == {"qtd_linhas": 8}
     assert priority_queue.search(2) == {"qtd_linhas": 7}
     with pytest.raises(IndexError):
         priority_queue.search(4)
-    
-  
 
     assert priority_queue.is_priority({"qtd_linhas": 3}) is True
     assert priority_queue.is_priority({"qtd_linhas": 5}) is False
